103929c66dbcd386825f72a0378bcf237a69d028
Fumycat <fumycat@gmail.com>
2020-11-23 13:56:28 +0700

    added LICK

--- meming.py
+++ meming.py
@@ -1,23 +1,24 @@
+import os
 from ast import literal_eval
 from utils import find_coeffs
 from PIL import Image, ImageOps
 
-template = '2b'
+template = 'lick'
 img = Image.open('input.png')
 
 bg = Image.open(f'templates/{template}/bg.png')
-mask = Image.open(f'templates/{template}/mask.png')
-# preview = Image.open(f'templates/{template}/preview.png')
+masks = [Image.open(f'templates/{template}/{m}') for m in filter(lambda x: x.startswith('mask'), os.listdir(f'templates/{template}'))]
 
 img = img.resize(bg.size)
 
 with open(f'templates/{template}/matrix.txt') as f:
-    matrix = literal_eval(f.read())
+    matrixs = literal_eval(f.read())
 
-coeffs = find_coeffs([(0, 0), (bg.size[0], 0), (bg.size[0], bg.size[1]), (0, bg.size[1])], matrix)
 
-img = img.transform(bg.size, Image.PERSPECTIVE, coeffs, Image.BICUBIC)
+for matrix, mask in zip(matrixs, masks):
+    coeffs = find_coeffs([(0, 0), (bg.size[0], 0), (bg.size[0], bg.size[1]), (0, bg.size[1])], matrix)
+    t = img.transform(bg.size, Image.PERSPECTIVE, coeffs, Image.BICUBIC)
+    bg.paste(t, (0, 0), mask)
 
-bg.paste(img, (0, 0), mask)
 # bg = ImageOps.autocontrast(bg, cutoff=3)
 bg.show()
